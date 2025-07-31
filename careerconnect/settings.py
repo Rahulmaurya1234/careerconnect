776e0e517cb62badb4132eacb963d220fd4a53f9
Rahul Maurya <rahul2003maurya@gmail.com>
2025-07-31 13:31:50 -0700

    Fix static files for production

--- careerconnect/settings.py
+++ careerconnect/settings.py
@@ -73,7 +73,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
+
 
 ROOT_URLCONF = 'careerconnect.urls'
 
@@ -141,10 +144,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+# settings.py
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
